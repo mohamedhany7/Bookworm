cd082da2bae84189b2235a414e8eddf729c479a4
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-07-13 22:32:09 +0200

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Mohamed Hany on 13/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
