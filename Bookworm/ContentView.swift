c1ef750ffeb8aee54f02f0130021ddc8a0e545b9
mohamedhany7 <mohameddhanyy7@gmail.com>
2022-07-14 04:50:24 +0200

    Adding RatingView

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -15,20 +15,38 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            Text("Count: \(books.count)")
-                .navigationTitle("Bookworm")
-                .toolbar {
-                    ToolbarItem(placement: .navigationBarTrailing) {
-                        Button {
-                            showingAddScreen.toggle()
+            List{
+                ForEach(books) { book in
+                        NavigationLink {
+                            Text(book.title ?? "Unknown Title")
                         } label: {
-                            Label("Add Book", systemImage: "plus")
+                            HStack {
+                                EmojiRatingView(rating: book.rating)
+                                    .font(.largeTitle)
+
+                                VStack(alignment: .leading) {
+                                    Text(book.title ?? "Unknown Title")
+                                        .font(.headline)
+                                    Text(book.author ?? "Unknown Author")
+                                        .foregroundColor(.secondary)
+                                }
+                            }
                         }
                     }
+            }
+            .navigationTitle("Bookworm")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        showingAddScreen.toggle()
+                    } label: {
+                        Label("Add Book", systemImage: "plus")
+                    }
                 }
-                .sheet(isPresented: $showingAddScreen) {
-                    AddBookView()
-                }
+            }
+            .sheet(isPresented: $showingAddScreen) {
+                AddBookView()
+            }
         }
     }
 }
